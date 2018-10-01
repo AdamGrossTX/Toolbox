f5da36b09ecf82bbdc96cab6a5323b7a5f25755e
Adam Gross <github@grosshome.com>
2018-10-01 14:01:20 -0500

    Fixes 1.5 and 1.6
            1.5 - Added fix for 2018-09 Windows10.0-KB4457190-x64.cab Setup Update not being classified correctly.
    
            1.6 - Corrected logic for ALL Updates

--- Windows 10 Servicing/Get-DynamicUpdates.ps1
+++ Windows 10 Servicing/Get-DynamicUpdates.ps1
@@ -26,10 +26,14 @@
         1.3 - Changed Property sort order for readability and added KBInfoURL. Removed Month param and collapsed folder structure to \version-arch instead of \version\month\arch\
 
         1.4 - Fixed Typo and Parameter Validation
+
+        1.5 - Added fix for 2018-09 Windows10.0-KB4457190-x64.cab Setup Update not being classified correctly.
+
+        1.6 - Corrected logic for ALL Updates
+
 #>
 
 #####################
-
 
 Param
 (
@@ -53,148 +57,111 @@ Param
     [string]$SiteCode,
 
     #Use this to download updates. Set to false to just generate the files list(s)
-    [Switch]$DownloadUpdates = $True
+    [Switch]$DownloadUpdates = $True,
+
+    #Use this to download updates. Set to false to just generate the files list(s)
+    [Switch]$ExcludeSuperseded
 
 )
 
 $Script:MasterList = @()
-
 $OSVersionList = @('1511','1607','1703','1709','1803','1809','Next')
-
 $OSArchList = @('x86','x64','ARM64')
-
 $AllDynamicUpdatesFilter = 'LocalizedCategoryInstanceNames = "Windows 10 Dynamic Update"'
-
 $AllDynamicUpdates = Get-WmiObject -ComputerName $SCCMServer -Class SMS_SoftwareUpdate -Namespace "root\SMS\Site_$($SiteCode)" -Filter $AllDynamicUpdatesFilter
 
+Function Process-Updates ($OSVersion,$OSArch) {
+    Write-Host "Processing $($OSVersion)-$($OSArch)"  -ForegroundColor Green
+    If($DownloadUpdates) {
+        $UpdatesPath = "$($RootFolder)\Updates\$($OSVersion)-$($OSArch)"
+        $DUSUPath = "$($UpdatesPath)\SetupUpdate"
+        $DUCUPath = "$($UpdatesPath)\ComponentUpdate"
 
-Function Process-Updates ($OSVersion,$OSArch)
-{
-        Write-Host "Processing $($OSVersion)-$($OSArch)"  -ForegroundColor Green
-        
-        If($DownloadUpdates)
-        {
-            $UpdatesPath = "$($RootFolder)\Updates\$($OSVersion)-$($OSArch)"
-
-            $DUSUPath = "$($UpdatesPath)\SetupUpdate"
-
-            $DUCUPath = "$($UpdatesPath)\ComponentUpdate"
-
-
-            if (!(Test-Path -path $DUSUPath)) {New-Item -path $DUSUPath -ItemType Directory}
-
-            if (!(Test-Path -path $DUCUPath)) {New-Item -path $DUCUPath -ItemType Directory}
-        }
-
-        $DownloadList = @()
-
-        $DisplayNameFilter = "*$($OSVersion)*$($OSArch)*"
-
-        $FilteredUpdateList = $AllDynamicUpdates | Where {$_.LocalizedDisplayName -like $DisplayNameFilter}
-
-        ForEach ($Update in $FilteredUpdateList)
-
-        {
-            $ContentIDs = Get-WmiObject -ComputerName $SCCMServer -Class SMS_CIToContent -Namespace "root\SMS\Site_$($SiteCode)" -Filter "CI_ID = $($Update.CI_ID)"
-
-            ForEach ($ContentID in $ContentIDs) {
-
-                $Content = Get-WmiObject -ComputerName $SCCMServer -Class SMS_CIContentFiles -Namespace "root\SMS\Site_$($SiteCode)" -Filter "ContentID = $($ContentID.ContentID)"
-
-                $DownloadList  += New-Object PSObject -Property:@{
-
-                'KB' = $Update.ArticleID;
-
-                #'CI_ID' = $Update.CI_ID;
-
-                'DisplayName' = $Update.LocalizedDisplayName;
-
-                #'ContentID' = $ContentIDs.ContentID;
-
-                'FileName' = $Content.FileName;
-
-                'URL' = $Content.SourceURL;
-
-                'KBInfoURL'= $Update.LocalizedInformativeURL;
-
-                'Type' = $Update.LocalizedDescription.Replace(":","")
-
-                }
-
-            }
-
-        }
-
-        ForEach ($File in $DownloadList)
-
-        {
-
-            $Path = $null
-
-            switch ($File.Type)
-
-            {
-                SetupUpdate {$Path = "$($DUSUPath)\$($File.FileName)"; break;}
-
-                ComponentUpdate {$Path = "$($DUCUPath)\$($File.FileName)"; break;}
-
-                Default {$Path = "$($UpdatesPath)\$($File.FileName)"; break;}
-
-            }
-
-            If($DownloadUpdates) {
-                Write-Host $Path -ForegroundColor Green
-                Invoke-WebRequest -Uri $File.URL -OutFile $Path
-            }
-
-        }
-
-        If($DownloadList)
-        {
-            $DownloadList | Sort-Object -Property Type, KB | Select KB, DisplayName, KBInfoURL, Type, FileName, URL | Export-Clixml -Path "$($RootFolder)\$($OSVersion)-$($OSArch)-Windows10DynamicUpdateList.XML"
-            $DownloadList | Sort-Object -Property Type, KB | Select KB, DisplayName, KBInfoURL, Type, FileName, URL | ConvertTo-Json | Out-File "$($RootFolder)\$($OSVersion)-$($OSArch)-Windows10DynamicUpdateList.json"
-
-            $Script:MasterList += $DownloadList
-        }
-
+        if (!(Test-Path -path $DUSUPath)) {New-Item -path $DUSUPath -ItemType Directory}
+        if (!(Test-Path -path $DUCUPath)) {New-Item -path $DUCUPath -ItemType Directory}
     }
+
+    $DownloadList = @()
+    $DisplayNameFilter = "*$($OSVersion)*$($OSArch)*"
+
+    If($ExcludeSuperseded) {
+        $FilteredUpdateList = $AllDynamicUpdates | Where {$_.LocalizedDisplayName -like $DisplayNameFilter -and $_.IsSuperseded -eq $False -and $_.IsLatest -eq $True}
+    } Else {
+        $FilteredUpdateList = $AllDynamicUpdates | Where {$_.LocalizedDisplayName -like $DisplayNameFilter}
+    }
+
+    ForEach ($Update in $FilteredUpdateList) {
+        $ContentIDs = Get-WmiObject -ComputerName $SCCMServer -Class SMS_CIToContent -Namespace "root\SMS\Site_$($SiteCode)" -Filter "CI_ID = $($Update.CI_ID)"
+        ForEach ($ContentID in $ContentIDs) {
+            $Content = Get-WmiObject -ComputerName $SCCMServer -Class SMS_CIContentFiles -Namespace "root\SMS\Site_$($SiteCode)" -Filter "ContentID = $($ContentID.ContentID)"
+            $DownloadList  += New-Object PSObject -Property:@{
+            'KB' = $Update.ArticleID;
+            #'CI_ID' = $Update.CI_ID;
+            'DisplayName' = $Update.LocalizedDisplayName;
+            #'ContentID' = $ContentIDs.ContentID;
+            'FileName' = $Content.FileName;
+            'URL' = $Content.SourceURL;
+            'KBInfoURL'= $Update.LocalizedInformativeURL;
+            'Type' = $Update.LocalizedDescription.Replace(":","")
+            'SuperSeded' = $Update.IsSuperseded
+            }
+        }
+    }
+
+    ForEach ($File in $DownloadList) {
+        $Path = $null
+        #Fix for September SetupUpdate not containing the correct text to classify propery.
+        If($File.FileName -eq "Windows10.0-KB4457190-x64.cab") { 
+            $Path = "$($DUSUPath)\$($File.FileName)"
+        }
+        Else {
+            switch ($File.Type) {
+                SetupUpdate {$Path = "$($DUSUPath)\$($File.FileName)"; break;}
+                ComponentUpdate {$Path = "$($DUCUPath)\$($File.FileName)"; break;}
+                Default {$Path = "$($UpdatesPath)\$($File.FileName)"; break;}
+            }
+        }
+
+        If($DownloadUpdates) {
+            Write-Host $Path -ForegroundColor Green
+            Invoke-WebRequest -Uri $File.URL -OutFile $Path
+        }
+    }
+
+    If($DownloadList)
+    {
+        $DownloadList | Sort-Object -Property Type, KB | Select KB, DisplayName, KBInfoURL, Type, FileName, URL | Export-Clixml -Path "$($RootFolder)\$($OSVersion)-$($OSArch)-Windows10DynamicUpdateList.XML"
+        $DownloadList | Sort-Object -Property Type, KB | Select KB, DisplayName, KBInfoURL, Type, FileName, URL | ConvertTo-Json | Out-File "$($RootFolder)\$($OSVersion)-$($OSArch)-Windows10DynamicUpdateList.json"
+        $Script:MasterList += $DownloadList
+    }
+}
 
 
 ## MAIN ##
 #################################### 
 
-If(!($OSVersion)) {
-
+If($OSVersion -eq 'ALL' -and $OSArch -eq 'ALL') {
     ForEach($Version in $OSVersionList) { 
-
-        If(($OSArch -eq "All"))
-        {
-            ForEach($Arch in $OSArchList) { 
-                Process-Updates $Version $Arch
-            }
-        }
-        Else
-        {
-            Process-Updates $Version $OSArch
+        ForEach($Arch in $OSArchList) { 
+            Process-Updates $Version $Arch
         }
     }
 }
+ElseIf ($OSVersion -eq 'ALL') {
+    ForEach($Version in $OSVersionList) { 
+        Process-Updates $Version $OSArch
+    }
+}
+ElseIf ($OSArch -eq 'ALL') {
+    ForEach($Arch in $OSArchList) { 
+        Process-Updates $OSVersion $Arch
+    }
+} 
 Else {
-
-    If(($OSArch -eq "All"))
-    {
-        ForEach($Arch in $OSArchList) { 
-            Process-Updates $OSVersion $Arch
-        }
-    }
-    Else
-    {
-        Process-Updates $OSVersion $OSArch
-    }
+    Process-Updates $OSVersion $OSArch
 }
 
 $Script:MasterList | Sort-Object -Property Type, KB | Select KB, DisplayName, KBInfoURL, Type, FileName, URL | Export-Clixml -Path "$($RootFolder)\All-Windows10DynamicUpdateList.XML"
-
 $Script:MasterList | Sort-Object -Property Type, KB | Select KB, DisplayName, KBInfoURL, Type, FileName, URL | ConvertTo-Json | Out-File "$($RootFolder)\All-Windows10DynamicUpdateList.json"
 
 ####################################    
