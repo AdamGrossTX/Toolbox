4e599e6dcf3942b3dcdd73e0f92936bc5033be80
Adam Gross <github@grosshome.com>
2019-07-23 09:44:54 -0500

    Update ListAdminServiceFunctions.ps1
    Added $metadata functions to see more info

--- ConfigMgr/AdminService/ListAdminServiceFunctions.ps1
+++ ConfigMgr/AdminService/ListAdminServiceFunctions.ps1
@@ -9,6 +9,17 @@ $Result.value.Name #Returns Function Names
 #>
 
 #Use this code for the 1906 TP AdminService
+#This code only works for the 1810 TP adminservice
+<#
+$Credential = Get-Credential
+$SCCMServerName = "localhost"
+$URL = "http://$($SCCMServerName):80/AdminService/v2/"
+$Result = Invoke-RestMethod -Method Get -Uri "$($URL)" -Credential $Credential
+$Result
+$Result.value.Name #Returns Function Names
+#>
+
+#Use this code for the 1906 TP AdminService
 ###############
 #Sample script for returning AdminService classes
 # By: Adam Gross
@@ -21,20 +32,54 @@ $SiteServer = "YourServerName"
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$ControllerUri = "https://$($SiteServer)/AdminService/v1.0/$metadata"
-$WMIUri = "https://$($SiteServer)/AdminService/wmi/$metadata"
+$ControllerUri = "https://$($SiteServer)/AdminService/v1.0/`$metadata"
+$WMIUri = "https://$($SiteServer)/AdminService/wmi/`$metadata"
 
 $agentsquery = New-Object System.Net.WebClient
-$agentsquery.Headers.Add('accept','application/json') 
+$agentsquery.Headers.Add('accept','application/xml') 
 $agentsquery.UseDefaultCredentials =$true
 
-$ControllerResults = $agentsquery.GetWebResponse($ControllerUri)
-$WMIResults = $agentsquery.DownloadString($WMIUri)
+[XML]$ControllerResults = $agentsquery.DownloadString($ControllerUri)
+[XML]$WMIResults = $agentsquery.DownloadString($WMIUri)
 
-$ControllerClasses = ($ControllerResults | ConvertFrom-Json).value
-$WMIClasses = ($WMIResults | ConvertFrom-Json).value
+
+#$ControllerClasses = ($ControllerResults | ConvertFrom-Json).value
+#$WMIClasses = ($WMIResults | ConvertFrom-Json).value
 
 #Write-Host "Controller Classes" -ForegroundColor Green
-$ControllerClasses | Out-GridView
+#$ControllerClasses | Out-GridView
 #Write-Host "WMI Classes" -ForegroundColor Green
-$WMIClasses | Out-GridView    
+#$WMIClasses | Out-GridView    
+
+#Controllers
+$ContClasses = $ControllerResults.Edmx.DataServices.Schema.EntityType
+$ContFunctions = $ControllerResults.Edmx.DataServices.Schema.Function
+$ContActions = $ControllerResults.Edmx.DataServices.Schema.Action
+$ContContainers = $ControllerResults.Edmx.DataServices.Schema.EntityContainer
+
+Write-Host "Classes" -ForegroundColor Green
+$ContClasses | Format-Table
+Write-Host "Functions" -ForegroundColor Green
+$ContFunctions| Format-Table
+Write-Host "Actions" -ForegroundColor Green
+$ContActions| Format-Table
+Write-Host "Containers" -ForegroundColor Green
+$ContContainers| Format-Table
+
+#WMI
+$wmiSchema = $WMIResults.Edmx.DataServices.Schema | Format-Table
+$wmiClasses = $WMIResults.Edmx.DataServices.Schema.EntityType | Format-Table
+$wmiComplexClasses = $WMIResults.Edmx.DataServices.Schema.ComplexType | Format-Table
+$wmiContainers = $WMIResults.Edmx.DataServices.Schema.EntityContainer | Format-Table
+
+Write-Host "Schema" -ForegroundColor Green
+$wmiSchema | Format-Table
+Write-Host "Classes" -ForegroundColor Green
+$wmiClasses | Format-Table
+Write-Host "ComplexClasses" -ForegroundColor Green
+$wmiComplexClasses | Format-Table
+Write-Host "Containers" -ForegroundColor Green
+$wmiContainers| Format-Table
+
+
+#You can add a foreach loop on each object to get the properties for each entity. 
