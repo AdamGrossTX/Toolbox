0c20559610cbf7028656eb3afc5f9afba6287f14
Adam Gross <github@grosshome.com>
2019-10-17 08:06:45 -0500

    Updated Sample Scripts
    Signed-off-by: Adam Gross <github@grosshome.com>

--- ConfigMgr/AdminService/ListAdminServiceFunctions.ps1
+++ ConfigMgr/AdminService/ListAdminServiceFunctions.ps1
@@ -19,7 +19,7 @@ $Result
 $Result.value.Name #Returns Function Names
 #>
 
-#Use this code for the 1906 TP AdminService
+#Use this code for the 1902/1902  AdminService
 ###############
 #Sample script for returning AdminService classes
 # By: Adam Gross
@@ -27,29 +27,26 @@ $Result.value.Name #Returns Function Names
 # https://www.asquaredozen.com
 ###############
 
+####Use this don't use these if you are using the Invoke-RestMethod cmdlets
+#[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#$agentsquery = New-Object System.Net.WebClient
+#$agentsquery.Headers.Add('accept','application/xml') 
+#$agentsquery.UseDefaultCredentials =$true
 
-$SiteServer = "YourServerName"
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#[XML]$ControllerResults = $agentsquery.DownloadString($ControllerUri)
+#[XML]$WMIResults = $agentsquery.DownloadString($WMIUri)
+################
+
+<#
+$SiteServer = "cm01.asd.net"
+$BaseUri = "https://$($SiteServer)/AdminService"
 
 $ControllerUri = "https://$($SiteServer)/AdminService/v1.0/`$metadata"
 $WMIUri = "https://$($SiteServer)/AdminService/wmi/`$metadata"
 
-$agentsquery = New-Object System.Net.WebClient
-$agentsquery.Headers.Add('accept','application/xml') 
-$agentsquery.UseDefaultCredentials =$true
-
-[XML]$ControllerResults = $agentsquery.DownloadString($ControllerUri)
-[XML]$WMIResults = $agentsquery.DownloadString($WMIUri)
-
-
-#$ControllerClasses = ($ControllerResults | ConvertFrom-Json).value
-#$WMIClasses = ($WMIResults | ConvertFrom-Json).value
-
-#Write-Host "Controller Classes" -ForegroundColor Green
-#$ControllerClasses | Out-GridView
-#Write-Host "WMI Classes" -ForegroundColor Green
-#$WMIClasses | Out-GridView    
+$WMIResults = Invoke-RestMethod -Method Get -Uri "$($WMIUri)" -UseDefaultCredentials
+$ControllerResults = Invoke-RestMethod -Method Get -Uri "$($ControllerUri)" -UseDefaultCredentials
 
 #Controllers
 $ContClasses = $ControllerResults.Edmx.DataServices.Schema.EntityType
@@ -67,19 +64,44 @@ Write-Host "Containers" -ForegroundColor Green
 $ContContainers| Format-Table
 
 #WMI
-$wmiSchema = $WMIResults.Edmx.DataServices.Schema | Format-Table
+$wmiSchema = $WMIResults.Edmx.DataServices.Schema | Format-List
 $wmiClasses = $WMIResults.Edmx.DataServices.Schema.EntityType | Format-Table
 $wmiComplexClasses = $WMIResults.Edmx.DataServices.Schema.ComplexType | Format-Table
 $wmiContainers = $WMIResults.Edmx.DataServices.Schema.EntityContainer | Format-Table
+#>
 
-Write-Host "Schema" -ForegroundColor Green
-$wmiSchema | Format-Table
-Write-Host "Classes" -ForegroundColor Green
-$wmiClasses | Format-Table
-Write-Host "ComplexClasses" -ForegroundColor Green
-$wmiComplexClasses | Format-Table
-Write-Host "Containers" -ForegroundColor Green
-$wmiContainers| Format-Table
+Function Get-AdminServiceDetails {
+    Param(
+        $ServerName,
+        $ClassType,
+        [switch]$Metadata=$true
+    )
 
+    $ClassTypeURI = Switch($ClassType)
+    {
+        "WMI" {"wmi"; Break;}
+        "Controller" {"v1.0"; Break;}
+    }
+
+    If($Metadata) {
+        $BaseURI = "https://{0}/AdminService/{1}/{2}" -f $SiteServer,$ClassTypeURI,"`$metadata"
+    }
+    Else {
+        $BaseURI = "https://{0}/AdminService/{1}/" -f $SiteServer,$ClassTypeURI
+    }
+
+    $Results = Invoke-RestMethod -Method Get -Uri "$($BaseURI)" -UseDefaultCredentials
+    $Results
+}
 
 #You can add a foreach loop on each object to get the properties for each entity. 
+
+#$ControllerClasses = ($ControllerResults | ConvertFrom-Json).value
+#$WMIClasses = ($WMIResults | ConvertFrom-Json).value
+
+#Write-Host "Controller Classes" -ForegroundColor Green
+#$ControllerClasses | Out-GridView
+#Write-Host "WMI Classes" -ForegroundColor Green
+#$WMIClasses | Out-GridView    
+
+& $Main
