5dfb4013ab8ad0429933c3dfd612c31befc7b810
Adam Gross <github@grosshome.com>
2019-12-14 21:59:35 -0600

    Cleanup
    Signed-off-by: Adam Gross <github@grosshome.com>

--- ConfigMgr/BootImage/ServicingBootWIM/AddZoomItKeys.ps1
+++
@@ -1,2 +0,0 @@
-New-ItemProperty -Path "HKCU:\Software\Sysinternals\ZoomIt" -Name "EulaAccepted" -Value 1 -PropertyType DWord -Force
-New-Item -Path "HKCU:\Software\Microsoft\Windows" -Name "Run" -Force
