bf186a028e55c5872ccfeca62f99e8c528a48026
Adam Gross <github@grosshome.com>
2019-12-14 23:09:53 -0600

    Updated Update-TSBootImageID
    Signed-off-by: Adam Gross <github@grosshome.com>

--- ConfigMgr/TaskSequence/Update-TSBootImageID.ps1
+++ ConfigMgr/TaskSequence/Update-TSBootImageID.ps1
@@ -1,3 +1,41 @@
+<#
+.SYNOPSIS
+    Bulk Update the BootImageID for Task Sequences in ConfigMgr
+.DESCRIPTION
+    Bulk Update the BootImageID for Task Sequences in ConfigMgr
+.PARAMETER SiteCode
+    ConfigMgr Site Code
+.PARAMETER SiteServer
+    ConfigMgr Site Server Name
+.PARAMETER OldBootImageID
+    ID of the current boot image that is being replaced
+.PARAMETER NewBootImageID
+    ID of the new boot image
+
+.NOTES
+  Version:          1.0
+  Author:           Adam Gross - @AdamGrossTX
+  GitHub:           https://www.github.com/AdamGrossTX
+  WebSite:          https://www.asquaredozen.com
+  Creation Date:    12/14/2019
+  
+.EXAMPLE
+    Update BootImageID
+    .\Update-TSBootImageID.ps1 $SiteCode "PS1" -ServerName "cm01.asd.net" -OldBootImageID "PS1000001" -NewBootImageID "PS1000002"
+
+.EXAMPLE
+    Update BootImageID with Splatting
+    $UpdateTSBootImageIDSplat = @{
+        SiteCode = "PS1" 
+        ServerName = "cm01.asd.net" 
+        OldBootImageID = "PS1000001" 
+        NewBootImageID = "PS1000002"
+    }
+
+    .\Update-TSBootImageID.ps1 $UpdateTSBootImageIDSplat
+
+ #>
+
 [cmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
@@ -6,7 +44,7 @@ param(
     
     [Parameter(Mandatory=$true)]
     [string]
-    $ProviderMachineName,
+    $ServerName,
     
     [Parameter(Mandatory=$true)]
     [string]
@@ -23,7 +61,7 @@ if((Get-Module ConfigurationManager) -eq $null) {
 }
 
 if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
-    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ServerName @initParams
 }
 Set-Location "$($SiteCode):\" @initParams
 
@@ -31,7 +69,7 @@ Set-Location "$($SiteCode):\" @initParams
 
 $TSList = Get-CMTaskSequence | Where-Object BootImageId -eq $OldBootImageID
 
-Write-Host "Updating the Boot Image on $($TSList.Count) Task Sequnces from $($OldBootImageID) to $($NewBootImageID)"
+Write-Host "Updating the Boot Image on $($TSList.Count) Task Sequences from $($OldBootImageID) to $($NewBootImageID)"
 
 $Count = 0
 ForEach ($TS in $TSList)
