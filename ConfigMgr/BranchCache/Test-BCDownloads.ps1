24cb064a0309071585e2a320c5fd8929f03d874f
AdamGrossTX <github@grosshome.com>
2022-03-27 15:30:19 -0500

    Refactored and cleaned

--- ConfigMgr/BranchCache/Test-BCDownloads.ps1
+++ ConfigMgr/BranchCache/Test-BCDownloads.ps1
@@ -1,5 +1,3 @@
-
-
 <#
 .SYNOPSIS
     tests BITS downloads to fins errors with BranchCache Content.
@@ -17,62 +15,125 @@
 # NOTE - If the impacted content is coming from MCC, this script doesn't work and I haven't sorted out why yet.
 
 #>
+[cmdletbinding()]
+param(
+    $Creds = (Get-Credential),
+    [string]$SourceFilePath = "C:\BCTest"
+)
 
+$script:tick    = " " + [char]0x221a
+$BCEnabledDest  = "$($SourceFilePath)\Enabled"
+$BCDisabledDest = "$($SourceFilePath)\Disabled"
+$fileListSource = "$($SourceFilePath)\FileList"
+$ExportFile     = "$($LocalExportPath)\Results_$(Get-Date -Format yyyyhhMM_HHmmss).CSV"
 
-$creds = Get-Credential
-$BCEnabledDest = "C:\BCTest\Enabled"
-$BCDisabledDest = "C:\BCTest\Disabled"
-$FileListSource = "C:\BCTest\FileList"
+New-Item -Path $BCEnabledDest -ItemType Directory -Force | Out-Null
+New-Item -Path $BCDisabledDest -ItemType Directory -Force | Out-Null
+New-Item -Path $fileListSource -ItemType Directory -Force | Out-Null
 
-New-Item -Path $BCEnabledDest -ItemType Directory -Force
-New-Item -Path $BCDisabledDest -ItemType Directory -Force
-New-Item -Path $FileListSource -ItemType Directory -Force
+function DownloadFiles {
+    param (
+        $fileObj,
+        $Dest
+    )
+    Write-Host "--$($fileObj.FileName)" -ForegroundColor Cyan -NoNewline
+    try {
+        $filePath = Join-Path -Path $Dest -ChildPath $fileObj.FileName
+        if ($fileObj.URL -like "HTTPS://*") {
+            $BitsOptions = @{
+                Source         = $fileObj.URL 
+                Destination    = $filePath
+                Credential     = $creds 
+                Authentication = "NTLM"
+            }
+        }
+        else {
+            $BitsOptions = @{
+                Source      = $fileObj.URL 
+                Destination = $filePath
+            }
+        }
 
+        $DLSeconds = Measure-Command { Start-BitsTransfer @BitsOptions } | Select-Object -ExpandProperty TotalSeconds
 
-if(Test-Path "$($FileListSource)\*.csv") {
-$FileList = Get-ChildItem -Path $FileListSource | Get-Content | ConvertFrom-CSV
-#Enable BC
-Enable-BCDistributed
-Clear-BCCache -Force
-
-$FileObjs = @{}
-
-ForEach($File in $FileList) {
-    $FileParts = $File.URL.Split("/")
-    $FileName = $FileParts[$FileParts.count-1]
-    $URL = $File.URL.Replace("/SCCM?","").Replace("SMS_DP_SMSPKG","NOCERT_SMS_DP_SMSPKG")
-
-    $FileObjs[$FileName] = [PSCustomObject]@{
-        FileName = $FileName
-        URL = $URL
-        EnabledHash = $null
-        DisabledHash = $null
+        if (Test-Path -Path $filePath) {
+            $Hash = Get-FileHash -Path $filePath | Select-Object -ExpandProperty Hash
+        }
+        Write-Host $tick -ForegroundColor Green
+        return $DLSeconds, $Hash
+    }
+    catch {
+        return $_
     }
 }
 
-ForEach($Key in $FileObjs.Keys) {
-    Start-BitsTransfer -Source $FileObjs[$key].URL -Destination $BCEnabledDest -Credential $creds -Authentication NTLM
-    $FilePath = Join-Path -Path $BCEnabledDest -ChildPath $FileObjs[$key].FileName
-    if(Test-Path -Path $FilePath) {
-        $FileObjs[$Key].EnabledHash = Get-FileHash -Path $FilePath | Select-Object -ExpandProperty Hash
+if (Test-Path "$($fileListSource)\*.csv") {
+    $fileList = Get-ChildItem -Path $fileListSource | Get-Content | ConvertFrom-CSV | Select-Object -Unique URL
+    Write-Host "Begin Processing $($fileList.Count) Files" -ForegroundColor Cyan
+    
+    #Enable BC
+    Write-Host "-Enabling Branchcache on the client in Distributed Mode." -ForegroundColor Cyan
+    Enable-BCDistributed | Out-Null
+    Write-Host "-Clearing Branchcache cache on the client." -ForegroundColor Cyan
+    Clear-BCCache -Force | Out-Null
+
+    $fileObjs = @{}
+
+    foreach ($file in $fileList) {
+        if ($file.URL -like "*SMS_DP*") {
+            $URL = $file.URL.Replace("/SCCM?", "").Replace("SMS_DP_SMSPKG", "NOCERT_SMS_DP_SMSPKG")
+            $fileParts = $URL.Split("/")
+            $fileName = $fileParts[$fileParts.count - 1]
+        }
+        else {
+            $URL = $file.URL
+            [regex]$regex = '(\/)(?!.*\/)(?<filename>.+)(\?)'
+            if ($URL -match $regex) {
+                $fileName = $matches.filename
+            }
+            else {
+                $fileParts = $URL.Split("/")
+                $fileName = $fileParts[$fileParts.count - 1]
+            }
+        }
+
+        $fileObjs[$fileName] = [PSCustomObject]@{
+            FileName       = $fileName
+            URL            = $URL
+            EnabledHash    = $null
+            DisabledHash   = $null
+            HashMatch      = $null
+            EnabledDLTime  = $null
+            DisabledDLTime = $null
+            TimeDiff       = $null
+        }
     }
-}
 
-#Disable BC
-Disable-BC -Force
-Clear-BCCache -Force
-
-ForEach($Key in $FileObjs.Keys) {
-    Start-BitsTransfer -Source $FileObjs[$key].URL -Destination $BCDisabledDest -Credential $creds -Authentication NTLM
-    $FilePath = Join-Path -Path $BCDisabledDest -ChildPath $FileObjs[$key].FileName
-    if(Test-Path -Path $FilePath) {
-        $FileObjs[$Key].DisabledHash = Get-FileHash -Path $FilePath | Select-Object -ExpandProperty Hash
+    foreach ($Key in $fileObjs.Keys) {
+        $fileObjs[$Key].EnabledDLTime, $fileObjs[$Key].EnabledHash = DownloadFiles -FileObj $fileObjs[$key] -Dest $BCEnabledDest
     }
-}
 
-    $FileObjs.values | Select-Object FileName,EnabledHash,DisabledHash
-    #TODO - Add a comparison function to find all entries where hashes don't match each other to make it easier to find impacted files
+    #Disable BC
+    Write-Host "-Disabling Branch4cache on the client." -ForegroundColor Cyan
+    Disable-BC -Force
+    Write-Host "-Clearing Branchcache cache on the client." -ForegroundColor Cyan
+    Clear-BCCache -Force
+    
+    foreach ($Key in $fileObjs.Keys) {
+        $fileObjs[$Key].DisabledDLTime, $fileObjs[$Key].DisabledHash = DownloadFiles -FileObj $fileObjs[$key] -Dest $BCDisabledDest
+        #FindMismatched hash values
+        $fileObjs[$Key].HashMatch = $fileObjs[$Key].EnabledHash -match $fileObjs[$Key].DisabledHash
+        #Find DL Time Difference 
+        $fileObjs[$Key].TimeDiff = $fileObjs[$Key].DisabledDLTime - $fileObjs[$Key].EnabledDLTime
+    }
+ 
+    $fileObjs.values | Export-Csv -Path $ExportFile
+
+    #Re-Enable BranchCache
+    Write-Host "-Re-Enabling Branchcache on the client in Distributed Mode." -ForegroundColor Cyan
+    Enable-BCDistributed | Out-Null
+
 }
 else {
-    Write-Host "No CSV Files Found. Run Get-BranchCacheError13FileList.ps1 on affected DP first."
+    Write-Host "No CSV Files Found. Run Get-BranchCacheError13FileList.ps1 on affected DP first." -ForegroundColor Yellow
 }
