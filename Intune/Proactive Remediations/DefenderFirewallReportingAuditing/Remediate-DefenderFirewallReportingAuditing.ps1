d29fcf68af25e7611143e4157923e16fc4a4a2da
Adam Gross <20917262+AdamGrossTX@users.noreply.github.com>
2022-04-25 18:04:18 -0500

    Add files via upload

---
+++ Intune/Proactive Remediations/DefenderFirewallReportingAuditing/Remediate-DefenderFirewallReportingAuditing.ps1
@@ -0,0 +1,30 @@
+param (
+    [switch]$remediate=$true
+)
+try {
+
+    $categories = "Filtering Platform Packet Drop,Filtering Platform Connection"
+    $current = auditpol /get /subcategory:"$($categories)" /r | ConvertFrom-Csv    
+    if ($current."Inclusion Setting" -ne "failure") {
+        if ($remediate.IsPresent) {
+            Write-Host "Remediating. No Auditing Enabled. $($current | ForEach-Object {$_.Subcategory + ":" + $_.'Inclusion Setting' + ";"})"
+            $output = auditpol /set /subcategory:"$($categories)" /failure:enable
+            if($output -eq "The command was successfully executed.") {
+                Write-Host "$($output)"
+                exit 0
+            }
+            else {
+                Write-Host "$($output)"
+                exit 1
+            }
+        }
+        else {
+            Write-Host "Remediation Needed. $($current | ForEach-Object {$_.Subcategory + ":" + $_.'Inclusion Setting' + ";"})."
+            exit 1
+        }
+    }
+
+}
+catch {
+    throw $_
+}
