e4bc4f8d0fed5cc4432b1ad1868c0eeec8f4f9d4
Adam Gross <20917262+AdamGrossTX@users.noreply.github.com>
2022-12-09 19:46:34 -0600

    Create Detect-WindowsUpdatePolicyKey.ps1

---
+++ Intune/Proactive Remediations/WindowsUpdatePolicyKey/Detect-WindowsUpdatePolicyKey.ps1
@@ -0,0 +1,19 @@
+param (
+    $RegPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate",
+    [switch]$Remediate
+)
+
+try {
+    if (Test-Path Registry::$RegPath -ErrorAction SilentlyContinue) {
+        if ($remediate) {
+            Remove-Item Registry::$RegPath -Recurse
+        }
+        else {
+            Write-Host "Reg Key Found. Remediation Needed."
+            Exit 1
+        }
+    }
+}
+catch {
+    throw $_
+}
