6eb34427339f41438e5362c200d2a29d20e56859
Adam Gross <20917262+AdamGrossTX@users.noreply.github.com>
2022-12-21 19:50:55 -0600

    Create Remediate-IntelSmartSound.ps1

---
+++ Intune/Proactive Remediations/IntelSmartSound/Remediate-IntelSmartSound.ps1
@@ -0,0 +1,24 @@
+param (
+    $incoming,
+    $DeviceName = "Intel® Smart Sound Technology for USB Audio",
+    [switch]$Remediate = $true
+)
+try {
+    $Devices = Get-PnpDevice -FriendlyName $DeviceName -ErrorAction SilentlyContinue
+    $Disabled = $true
+    foreach($Device in $Devices) {
+        if($Device.Problem -ne 'CM_PROB_DISABLED' -and $Device.Problem -ne 'CM_PROB_PHANTOM') {
+            $Disabled = $false
+            if($Remediate.IsPresent) {
+                $Device | Disable-PnpDevice -Confirm:$false -ErrorAction SilentlyContinue
+                Get-PnpDevice -InstanceId $Device.InstanceId -ErrorAction SilentlyContinue
+            }
+        }
+    }
+    if(-not $Remediate) {
+        return $Disabled
+    }
+}
+catch {
+    throw $_
+}
