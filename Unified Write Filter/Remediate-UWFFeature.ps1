fb91401a5bc7ac3226167276e1a31a03f6cffa79
Adam Gross <20917262+AdamGrossTX@users.noreply.github.com>
2023-03-13 11:52:40 -0500

    Add files via upload

---
+++ Unified Write Filter/Remediate-UWFFeature.ps1
@@ -0,0 +1,22 @@
+param(
+    [switch]$remediate = $true
+)
+try {
+    $UWFFeatureState = (Get-WindowsOptionalFeature -FeatureName "Client-UnifiedWriteFilter" -Online -ErrorAction SilentlyContinue).State
+    if ($UWFFeatureState -eq "Disabled") {
+        if ($Remediate.IsPresent) {
+            $Return = Enable-WindowsOptionalFeature -Online -FeatureName "Client-UnifiedWriteFilter" -NoRestart -All -ErrorAction Continue
+            Write-Host "Enabled UWF Feature"
+        }
+        else {
+            Write-Warning "The Unified Write Filter Feature is currently disabled. Use Enable-UWFFeature to enable it before useing this module."
+            exit 1
+        }
+    }
+    else {
+        Write-Host "Unified Write Filter Already Enabled."
+    }
+}
+catch {
+    throw $_
+}
