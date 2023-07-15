44bac781a65caa028a76431fdb8685403a5832a1
Adam Gross <20917262+AdamGrossTX@users.noreply.github.com>
2023-07-14 20:06:50 -0500

    Create Detect-DefaultIMApp.ps1

---
+++ Intune/Proactive Remediations/DefaultIMApp/Detect-DefaultIMApp.ps1
@@ -0,0 +1,20 @@
+#Thanks @matbg for the tip
+#https://twitter.com/matbg/status/1679760512874053632?s=46&t=wqKIfQ91Lwnlhukm2orOpQ
+#Run as logged on user.
+#Detect the selected IM Provider
+#Teams = Microsoft Teams
+#MSTeams = Nee Teams preview appx app
+#Skype = Shame on you. Time to upgrade!
+try {
+    $DefaultIMApp = Get-ItemProperty -Path registry::"HKEY_CURRENT_USER\Software\IM Providers" | Select-Object -ExpandProperty DefaultIMApp
+    if($DefaultIMApp) {
+        Write-Host $DefaultIMApp
+    }
+    else {
+        Write-Host "NONE"
+    }
+    exit 0
+}
+catch {
+
+}
